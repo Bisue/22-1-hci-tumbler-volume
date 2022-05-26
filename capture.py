9ab609e4a45fabb621f0c61d35a14ad342633f92
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-05-27 01:52:48 +0900

    가이드 이미지도 저장

--- capture.py
+++ capture.py
@@ -17,6 +17,7 @@ while True:
 
     # 마커 크기 계산
     marker_ratio = 54 / 86  # 마커 가로/세로 비율 (민증: 5.6cm/8.6cm)
+    # marker_ratio = 5398 / 8560  # 마커 가로/세로 비율 (카드(ID-1 규격): 5.398cm/8.560cm)
     marker_width = 200  # 마커 width (변경)
     marker_height = marker_width * marker_ratio  # 마커 height
 
@@ -29,13 +30,14 @@ while True:
     guide_image = original_image.copy()
     cv2.rectangle(guide_image, marker_p1, marker_p2, (255, 0, 0), 2, cv2.LINE_AA)
 
-    # 타이머 실행중이면 숫자 Display
-    if timer.is_capturing() and not timer.done():
+    # 타이머 실행중이면 처리하고 숫자 Display
+    if timer.is_capturing():
         tr = timer.process_timer()
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(
-            guide_image, str(tr), (50, 100), font, 4, (0, 255, 255), 4, cv2.LINE_AA,
-        )
+        if not timer.done():
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(
+                guide_image, str(tr), (50, 100), font, 4, (0, 255, 255), 4, cv2.LINE_AA,
+            )
 
     # 가이드 이미지 출력
     cv2.imshow("Guide", guide_image)
@@ -49,10 +51,13 @@ while True:
         cv2.destroyAllWindows()
         # 캡쳐 후 출력/저장
         cv2.imshow("Original", original_image)
-        cv2.imwrite("./output.png", original_image)
+        cv2.imwrite("./outputs/original.png", original_image)
+        # 가이드 캡쳐 후 출력/저장
+        cv2.imshow("Guide", guide_image)
+        cv2.imwrite("./outputs/guide.png", guide_image)
         # (테스트) filled contours 캡쳐 후 출력/저장
         cv2.imshow("Filled Contours", tumbler)
-        cv2.imwrite("./output_filled_contours.png", tumbler)
+        cv2.imwrite("./outputs/filled_contours.png", tumbler)
         break
 
     # 키보드 입력
